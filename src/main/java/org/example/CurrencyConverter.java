02d098da9fb522e40511b8477679d5b801b03566
asaf29999 <asaf29999@gmail.com>
2024-08-05 17:28:08 +0300

    working beta

---
+++ src/main/java/org/example/CurrencyConverter.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class CurrencyConverter {
+
+    private String key = "cur_live_YdskGGNzUwnLGyhdbzn5H2y2jFBEr8JLIHfga710";
+
+    public String convert(String from, String to, String amount) {
+        double convertedAmount = 0;
+        try {
+            String route = "https://api.currencyapi.com/v3/latest?apikey=" + key + "&base_currency=" + from + "&currencies=" + to;
+
+            URL url = new URL(route);
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
+
+            JsonParser jp = new JsonParser();
+            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+            JsonObject jsonobj = root.getAsJsonObject();
+
+            // Extract the value from the JSON response
+            JsonObject dataObject = jsonobj.getAsJsonObject("data");
+            JsonObject currencyObject = dataObject.getAsJsonObject(to);
+            double value = currencyObject.get("value").getAsDouble();
+
+            // Convert the amount to double and perform the conversion
+            double amountDouble = Double.parseDouble(amount);
+            convertedAmount = amountDouble * value;
+
+            // Return the converted amount as a string
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return String.valueOf(convertedAmount);
+    }
+
+    public static void main(String[] args) {
+
+        CurrencyConverter converter = new CurrencyConverter();
+        String result = converter.convert("USD", "AUD", "100");
+        System.out.println("Converted amount: " + result);
+
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,12 @@
 package org.example;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Input input =new Input();
         input.start();
 
